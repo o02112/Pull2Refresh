4ac45dd673b6df87867aeaf534f8f7c7af139f47
liulang <liulang@caitu99.com>
2016-01-25 16:46:56 +0800

    language chinese remove

--- data.php
+++ data.php
@@ -1,10 +1,10 @@
 <?php
 	$arr = array();
-	$arr['plist'][] = array('title'=>'产品名称', 'price'=>19.5, 'img'=>'images/img.png');
+	$arr['plist'][] = array('title'=>'product name1', 'price'=>19.5, 'img'=>'images/img.png');
 	$arr['plist'][] = array('title'=>$_POST['key'], 'price'=>$_POST['va'], 'img'=>'images/img.png');
-	$arr['plist'][] = array('title'=>'产品名称', 'price'=>19.5, 'img'=>'images/img.png');
+	$arr['plist'][] = array('title'=>'product name2', 'price'=>10.5, 'img'=>'images/img.png');
 	$arr['plist'][] = array('title'=>$_POST['key'], 'price'=>$_POST['va'], 'img'=>'images/img.png');
-	$arr['plist'][] = array('title'=>'产品名称', 'price'=>19.5, 'img'=>'images/img.png');
+	$arr['plist'][] = array('title'=>'product name3', 'price'=>13.0, 'img'=>'images/img.png');
 	
 	sleep(2);
 	echo json_encode($arr);
